3e1cff42920c7974d87bc340568e106987ab48f8
Adam Cooper <adam@sequence7.net>
2013-03-02 13:34:01 -0700

    Added source editing

--- Scripts/app.js
+++ Scripts/app.js
@@ -23,20 +23,100 @@ var newsItem = function (url, title, details, imageUrl, time, columnSize) {
     };
 };
 
+
+var source = function (name, url, mappingFunction, width, ids) {
+    var _this = this;
+
+    _this.name = name;
+    _this.url = url;
+    _this.mappingFunction = mappingFunction;
+    _this.width = width;
+    _this.ids = ko.observableArray(ids);
+}
+
+
+// Class to represent sources
+var sources = function () {
+    var _this = this;
+
+    _this.twitterIds;
+    _this.youTubeIds;
+    _this.soundCloudIds;
+        
+    _this.getArrayFromStorage = function (id) {
+        var item = localStorage.getItem(id);
+
+        if (item === null || item == "undefined") {
+            return [];
+        } else {
+            return JSON.parse(item);
+        }
+    }
+
+    _this.init = function () {
+        _this.twitterIds = ko.observableArray(_this.getArrayFromStorage("twitterIds"));
+        _this.youTubeIds = ko.observableArray(_this.getArrayFromStorage("youTubeIds"));
+        _this.soundCloudIds = ko.observableArray(_this.getArrayFromStorage("soundCloudIds"));
+
+        // If there aren't any sources then show a welcome message and set some defaults
+        if (_this.twitterIds().length == 0 && _this.youTubeIds().length == 0 && _this.soundCloudIds().length == 0) {
+            alert("Looks like you haven't set up any sources, don't worry I've added Ninja Tune as a default, click edit if you want to change your sources");
+
+            _this.twitterIds.push("ninjatunehq");
+            _this.youTubeIds.push("ninja000");
+            _this.soundCloudIds.push("ninja-tune");
+        }
+    }
+
+    _this.save = function () {
+        localStorage.setItem("twitterIds", JSON.stringify(_this.twitterIds()));
+        localStorage.setItem("youTubeIds", JSON.stringify(_this.youTubeIds()));
+        localStorage.setItem("soundCloudIds", JSON.stringify(_this.soundCloudIds()));
+
+        $("#sources").slideUp();
+    }
+
+    _this.show = function () {
+        $("#sources").slideDown();
+    }
+
+    _this.addTwitter = function () {
+        _this.twitterIds.push("");
+    };
+
+    _this.removeTwitter = function (twitter) {
+        _this.twitterIds.remove(twitter);
+    };
+
+    _this.addYouTube = function () {
+        _this.youTubeIds.push("");
+    }
+
+    _this.removeYouTube = function (youTube) {
+        _this.youTubeIds.remove(youTube);
+    }
+
+    _this.addSoundCloud = function () {
+        _this.soundCloudIds.push("");
+    }
+
+    _this.removeSoundCloud = function (soundCloud) {
+        _this.soundCloudIds.remove(soundCloud);
+    }
+};
+
 // Overall viewmodel for this screen, along with initial state
 function appViewModel() {
     var _this = this;
 
-    // Specify user ids to be queried
-    _this.twitterIds = ["ninjatunehq"];
-    _this.youTubeIds = ["ninja000"];
-    _this.soundCloudIds = ["ninja-tune"];
+    _this.sources = new sources();
+    _this.sources.init();
 
     _this.pendingCallbacks = 0;
 
     _this.newsItems = ko.observableArray([]);
 
-    _this.twitterIds.forEach(function (id) {
+    _this.sources.twitterIds().forEach(function (id) {
         _this.pendingCallbacks++;
 
         $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + id + "&include_rts=true&count=40&callback=?", function (data) {
@@ -60,7 +140,7 @@ function appViewModel() {
         });
     });
 
-    _this.youTubeIds.forEach(function (id) {
+    _this.sources.youTubeIds().forEach(function (id) {
         _this.pendingCallbacks++;
 
         $.getJSON("http://gdata.youtube.com/feeds/api/users/" + id + "/uploads?v=2&alt=jsonc", function (data) {
@@ -77,7 +157,7 @@ function appViewModel() {
         });
     });
 
-    _this.soundCloudIds.forEach(function (id) {
+    _this.sources.soundCloudIds().forEach(function (id) {
         _this.pendingCallbacks++;
 
         $.getJSON("https://api.soundcloud.com/users/" + id + "/tracks.json?client_id=0f09d82872276292dad27414f7d88531", function (data) {
