a19d64a9a7ca8d70a52bdd24284eae35589f73a1
Adam Cooper <adam@sequence7.net>
2013-03-15 21:47:30 -0600

    Refactoring and enabled editting of config

--- Scripts/app.js
+++ Scripts/app.js
@@ -23,160 +23,120 @@ var newsItem = function (url, title, details, imageUrl, time, columnSize) {
     };
 };
 
-
-var source = function (name, url, mappingFunction, width, ids) {
+// Class to represent a news source
+var source = function (name, url, mappingFunction) {
     var _this = this;
 
     _this.name = name;
     _this.url = url;
     _this.mappingFunction = mappingFunction;
-    _this.width = width;
-    _this.ids = ko.observableArray(ids);
+    _this.ids = ko.observableArray();
+    _this.idToAdd = ko.observable("");
+
+    _this.addId = function () {
+        if ($.inArray(_this.idToAdd(), _this.ids()) == -1) {
+            _this.ids.push(_this.idToAdd());
+            _this.idToAdd("");
+        }
+    }
+
+    _this.removeId = function (id) {
+        _this.ids.remove(id);
+    }
 }
 
-
-// Class to represent sources
-var sources = function () {
+var config = function () {
     var _this = this;
 
-    _this.twitterIds;
-    _this.youTubeIds;
-    _this.soundCloudIds;
-        
-    _this.getArrayFromStorage = function (id) {
-        var item = localStorage.getItem(id);
+    _this.title = ko.observable("");
 
-        if (item === null || item == "undefined") {
-            return [];
-        } else {
-            return JSON.parse(item);
-        }
-    }
+    _this.sources = [
+        new source(
+            "YouTube",
+            "http://gdata.youtube.com/feeds/api/users/{id}/uploads?v=2&alt=jsonc",
+            function (data, newsItems) {
+                $.each(data.data.items, function (key, value) {
+                    newsItems.push(new newsItem(
+                        value.player.default,
+                        value.title,
+                        value.description,
+                        value.thumbnail.hqDefault,
+                        value.uploaded,
+                        "480px"));
+                });
+            }),
+        new source(
+            "Twitter",
+            "https://api.twitter.com/1/statuses/user_timeline.json?screen_name={id}&include_rts=true&count=40&callback=?",
+            function (data, newsItems) {
+                $.each(data, function (key, value) {
+                    var createdAt = value.created_at;
 
-    _this.init = function () {
-        _this.twitterIds = ko.observableArray(_this.getArrayFromStorage("twitterIds"));
-        _this.youTubeIds = ko.observableArray(_this.getArrayFromStorage("youTubeIds"));
-        _this.soundCloudIds = ko.observableArray(_this.getArrayFromStorage("soundCloudIds"));
-
-        // If there aren't any sources then show a welcome message and set some defaults
-        if (_this.twitterIds().length == 0 && _this.youTubeIds().length == 0 && _this.soundCloudIds().length == 0) {
-            alert("Looks like you haven't set up any sources, don't worry I've added Ninja Tune as a default, click edit if you want to change your sources");
-
-            _this.twitterIds.push("ninjatunehq");
-            _this.youTubeIds.push("ninja000");
-            _this.soundCloudIds.push("ninja-tune");
-        }
-    }
+                    // IE can't handle twitter's date format, so we hack it up a bit
+                    // this probably doesn't take into account international time offfsets TODO: Fix
+                    if (new Date(createdAt) == "Invalid Date") {
+                        createdAt = createdAt.replace(/\+\d{4}\s/, "");
+                    }
+                    newsItems.push(new newsItem(
+                        "http://twitter.com/", // TODO: Have a sensible url
+                        "",
+                        value.text,
+                        value.user.profile_image_url,
+                        createdAt,
+                        "160px"));
+                });
+            }),
+        new source(
+            "SoundCloud",
+            "https://api.soundcloud.com/users/{id}/tracks.json?client_id=0f09d82872276292dad27414f7d88531",
+            function (data, newsItems) {
+                $.each(data, function (key, value) {
+                    newsItems.push(new newsItem(
+                        value.permalink_url,
+                        value.title,
+                        value.description,
+                        value.artwork_url,
+                        value.created_at,
+                        "320px"));
+                });
+            })
+    ];
 
     _this.save = function () {
-        localStorage.setItem("twitterIds", JSON.stringify(_this.twitterIds()));
-        localStorage.setItem("youTubeIds", JSON.stringify(_this.youTubeIds()));
-        localStorage.setItem("soundCloudIds", JSON.stringify(_this.soundCloudIds()));
+        // Save the title
+        localStorage.setItem("title", _this.title());
 
-        $("#sources").slideUp();
-    }
-
-    _this.show = function () {
-        $("#sources").slideDown();
-    }
-
-    _this.addTwitter = function () {
-        _this.twitterIds.push("");
+        // Save the ids for each  source
+        _this.sources.forEach(function (sourceItem) {
+            localStorage.setItem(sourceItem.name, JSON.stringify(sourceItem.ids()));
+        });
     };
 
-    _this.removeTwitter = function (twitter) {
-        _this.twitterIds.remove(twitter);
-    };
+    _this.load = function () {
+        // Load the title
+        _this.title(localStorage.getItem("title"));
 
-    _this.addYouTube = function () {
-        _this.youTubeIds.push("");
-    }
+        // Load the ids for each source
+        _this.sources.forEach(function (sourceItem) {
+            var item = localStorage.getItem(sourceItem.name);
 
-    _this.removeYouTube = function (youTube) {
-        _this.youTubeIds.remove(youTube);
+            if (item !== null && item != "undefined") {
+                sourceItem.ids = ko.observableArray(JSON.parse(item));
+            }
+        });
     }
-
-    _this.addSoundCloud = function () {
-        _this.soundCloudIds.push("");
-    }
-
-    _this.removeSoundCloud = function (soundCloud) {
-        _this.soundCloudIds.remove(soundCloud);
-    }
-};
+}
 
 // Overall viewmodel for this screen, along with initial state
 function appViewModel() {
     var _this = this;
 
-    _this.sources = new sources();
-    _this.sources.init();
-
-    _this.pendingCallbacks = 0;
-
     _this.newsItems = ko.observableArray([]);
-
-    _this.sources.twitterIds().forEach(function (id) {
-        _this.pendingCallbacks++;
-
-        $.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + id + "&include_rts=true&count=40&callback=?", function (data) {
-            $.each(data, function (key, value) {
-                var createdAt = value.created_at;
-
-                // IE can't handle twitter's date format, so we hack it up a bit
-                // this probably doesn't take into account international time offfsets TODO: Fix
-                if (new Date(createdAt) == "Invalid Date") {
-                    createdAt = createdAt.replace(/\+\d{4}\s/, "");
-                }
-                _this.newsItems.push(new newsItem(
-                    "http://twitter.com/" + id,
-                    "",
-                    value.text,
-                    value.user.profile_image_url,
-                    createdAt,
-                    "160px"));
-            });
-            _this.callbackComplete();
-        });
-    });
-
-    _this.sources.youTubeIds().forEach(function (id) {
-        _this.pendingCallbacks++;
-
-        $.getJSON("http://gdata.youtube.com/feeds/api/users/" + id + "/uploads?v=2&alt=jsonc", function (data) {
-            $.each(data.data.items, function (key, value) {
-                _this.newsItems.push(new newsItem(
-                    value.player.default,
-                    value.title,
-                    value.description,
-                    value.thumbnail.hqDefault,
-                    value.uploaded,
-                    "480px"));
-            });
-            _this.callbackComplete();
-        });
-    });
-
-    _this.sources.soundCloudIds().forEach(function (id) {
-        _this.pendingCallbacks++;
-
-        $.getJSON("https://api.soundcloud.com/users/" + id + "/tracks.json?client_id=0f09d82872276292dad27414f7d88531", function (data) {
-            $.each(data, function (key, value) {
-                _this.newsItems.push(new newsItem(
-                    value.permalink_url,
-                    value.title,
-                    value.description,
-                    value.artwork_url,
-                    value.created_at,
-                    "320px"));
-            });
-            _this.callbackComplete();
-        });
-    });
+    _this.pendingCallbacks = 0;
+    _this.config = new config();
 
     _this.callbackComplete = function () {
         _this.pendingCallbacks--;
-
         // If all the callbacks are complete
         if (_this.pendingCallbacks == 0) {
             // Sort the news items
@@ -195,4 +155,40 @@ function appViewModel() {
             });
         }
     };
-};
+
+    _this.saveConfig = function () {
+        _this.config.save();
+        $("#sources").slideUp();
+        _this.init();
+    };
+
+    _this.editConfig = function () {
+        $("#sources").slideDown();
+    };
+
+    _this.init = function () {
+        _this.config.load();
+
+        // Loop through each of the sources
+        _this.config.sources.forEach(function (newsSource) {
+            // Loop through each id of a source
+            newsSource.ids().forEach(function (id) {
+                _this.pendingCallbacks++;
+                var url = newsSource.url.replace(/\{id\}/, id);
+                $.getJSON(url, function (data) {
+                    newsSource.mappingFunction(data, _this.newsItems);
+                    _this.callbackComplete();
+                });
+            });
+        });
+
+        // If there aren't any sources then show a welcome message and set some defaults
+        //if (_this.twitterIds().length == 0 && _this.youTubeIds().length == 0 && _this.soundCloudIds().length == 0) {
+        //    alert("Looks like you haven't set up any sources, don't worry I've added Ninja Tune as a default, click edit if you want to change your sources");
+
+        //    _this.twitterIds.push("ninjatunehq");
+        //    _this.youTubeIds.push("ninja000");
+        //    _this.soundCloudIds.push("ninja-tune");
+        //}
+    };
+}
